c455b5de3619b801b7f68f09ebd7e560d196206f
VoyakaGOD <ab3000645@gmail.com>
2024-09-20 19:15:07 +0300

    little improvements

--- cell.py
+++ cell.py
@@ -1,11 +1,13 @@
 from stop import stop
-from constants import EXPECTED_CELLS_SIMILARITY
-from diff import get_editorial_matrix
+from constants import *
+from diff import *
 import json
 
 class Cell:
     def __init__(self, jsonObj):
-        self.type = jsonObj["type"]
+        if ("cell_type" not in jsonObj) or ("source" not in jsonObj):
+            stop("Bad cell(has no type or no source)!")
+        self.type = jsonObj["cell_type"]
         self.source = jsonObj["source"]
         if "outputs" in jsonObj:
             self.output = jsonObj["outputs"]
@@ -15,10 +17,10 @@ class Cell:
             return False
         if self.type != other.type:
             return False
-        distance = get_editorial_matrix(self.source, other.source)[len(self.source), len(other.source)]
+        distance = get_editorial_matrix(self.source, other.source)[len(self.source)][len(other.source)]
         return distance <= EXPECTED_CELLS_SIMILARITY * (len(self.source) + len(other.source))
 
-def get_cells(path) -> list[Cell]:
+def get_cells(path : str) -> list[Cell]:
     data = None
     try:
         with open(path) as file:
@@ -28,3 +30,6 @@ def get_cells(path) -> list[Cell]:
     if "cells" not in data:
         stop("[" + path + "] is not a notebook file!")
     return [Cell(item) for item in data["cells"]]
+
+def show_cells_diff(old : Cell, new : Cell):
+    pass
